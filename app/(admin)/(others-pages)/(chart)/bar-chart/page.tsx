86d3a9360a791ed19ae200b999cf1fc20b541151
maearon <manhng132@gmail.com>
2025-08-02 11:46:11 +0700

    BarChart fix

--- app/(admin)/(others-pages)/(chart)/bar-chart/page.tsx
+++ app/(admin)/(others-pages)/(chart)/bar-chart/page.tsx
@@ -12,7 +12,7 @@ export async function generateMetadata(): Promise<Metadata> {
 };
 }
 
-export default function page() {
+export default function BarChart() {
   return (
     <div>
       <PageBreadcrumb pageTitle="Bar Chart" />
